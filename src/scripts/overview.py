0e87a91dfb14487edeac56dadc060ecc50eaf3bf
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-16 13:26:44 +0100

    added script for overview pdf

---
+++ src/scripts/overview.py
@@ -0,0 +1,21 @@
+from graphviz import Digraph
+import paths
+
+g = Digraph(format="pdf")
+
+# Nodes
+g.node("data", "Data")
+g.node("scripts", "Scripts")
+g.node("figures", "Figures")
+g.node("tex", "LaTeX manuscript")
+g.node("pdf", "Article PDF")
+
+# Workflow
+g.edge("data", "scripts")
+g.edge("scripts", "figures")
+g.edge("figures", "tex")
+g.edge("tex", "pdf")
+
+g.render("overview", view=True)
+
+g.render(paths.figures / 'flowchart', format='pdf', cleanup=True)
