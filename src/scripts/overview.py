6881371dc6731aead76c706572feebf95542050d
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-16 14:03:05 +0100

    edit overview

--- src/scripts/overview.py
+++ src/scripts/overview.py
@@ -15,18 +15,18 @@ with g.subgraph(name='cluster_group1') as c1:
     c1.attr(label="Overleaf", fontsize=fs_big, labelloc="t", labeljust="l")
     c1.node('A', 'manuscript', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
 with g.subgraph(name='cluster_group2') as c2:
-    c2.attr(label="Overleaf", fontsize=fs_big, labelloc="t", labeljust="l")
+    c2.attr(label="GitHub", fontsize=fs_big, labelloc="t", labeljust="l")
     c2.node('B', 'analysis scripts', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c2.node('C', 'figure scripts', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c2.node('D', 'environment', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c2.node('E', 'rules', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c2.node('F', 'manuscript', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
 with g.subgraph(name='cluster_group3') as c3:
-    c3.attr(label="Overleaf", fontsize=fs_big, labelloc="t", labeljust="l")
+    c3.attr(label="Zenodo", fontsize=fs_big, labelloc="t", labeljust="l")
     c3.node('G', 'static dataset', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c3.node('H', 'dynamic dataset', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
 with g.subgraph(name='cluster_group4') as c4:
-    c4.attr(label="Overleaf", fontsize=fs_big, labelloc="t", labeljust="l")
+    c4.attr(label="GitHub action", fontsize=fs_big, labelloc="t", labeljust="l")
     c4.node('I', 'manuscript', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
     c4.node('J', 'figures', shape='box', fontsize=fs, style="filled", fillcolor="lightblue")
 # g.node("data", "Data")
