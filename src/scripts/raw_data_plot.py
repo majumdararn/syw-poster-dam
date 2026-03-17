8263a90fd22f171f02854e075f241556b938a474
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-17 17:38:03 +0100

    adding raw data plot script

--- src/scripts/raw_data_plot.py
+++ src/scripts/raw_data_plot.py
@@ -12,39 +12,31 @@ import paths
 tar_path=paths.data / 'data.tar.gz'
 tar=tarfile.open(tar_path, 'r:gz')
 
-# # cwd
-# print('Getting current working dir (cwd):')
-# cwd=os.getcwd()
-# print('cwd: ', cwd)
-
-# # create dir for savinf reduced data
-# final_dir='data_red'
-# os.makedirs(final_dir, exist_ok=True)
-
-# # open tarfile
-# tar=tarfile.open('data.tar.gz', 'r:gz')
-# data=tar.extractfile('data')
-
 # read trail
 exp_idx=0
-while os.path.exists(tar.extractfile(f'data/exp_{exp_idx}')):
-    # print(f'exists exp data/exp_{exp_idx}')
-    # print(f'reading data.txt from data/exp_{exp_idx}')
-    txt_file_path=tar.extractfile(f'data/exp_{exp_idx}/data.txt')
-    # with open(txt_file_path, "r") as f:
-    #     # Read the first two lines
-    #     _ = f.readline()  # first line (skip)
-    #     second_line = f.readline()  # second line
-    #     alpha_val_str=second_line[9:]
-    #     alpha_val=float(alpha_val_str)
-    #     print(f'Rotation angle is: {alpha_val}')
-    data=np.loadtxt(txt_file_path, comments='#', dtype='float')
-    x=data[:,0]
-    y=data[:,1]
-    x_noise=data[:,2]
-    y_noise=data[:,3]
-    # x_red, y_red=data_angle_cor(x_noise, y_noise, alpha_val)
-    plt.plot(x_noise,y_noise,'o')
+
+while True:
+    try:
+        txt_file_path = tar.extractfile(f'data/exp_{exp_idx}/data.txt')
+        data = np.loadtxt(txt_file_path, comments='#', dtype=float)
+        x=data[:,0]
+        y=data[:,1]
+        x_noise=data[:,2]
+        y_noise=data[:,3]
+        # x_red, y_red=data_angle_cor(x_noise, y_noise, alpha_val)
+        plt.plot(x_noise,y_noise,'o')
+        exp_idx += 1
+    except KeyError:
+        break
+# while os.path.exists(tar.extractfile(f'data/exp_{exp_idx}')):
+#     txt_file_path=tar.extractfile(f'data/exp_{exp_idx}/data.txt')
+#     data=np.loadtxt(txt_file_path, comments='#', dtype='float')
+#     x=data[:,0]
+#     y=data[:,1]
+#     x_noise=data[:,2]
+#     y_noise=data[:,3]
+#     # x_red, y_red=data_angle_cor(x_noise, y_noise, alpha_val)
+#     plt.plot(x_noise,y_noise,'o')
     # plt.show()
     # final_data_dir=f'exp_{exp_idx}_red'
     # final_data_dir=os.path.join(final_dir, final_data_dir)
@@ -54,7 +46,7 @@ while os.path.exists(tar.extractfile(f'data/exp_{exp_idx}')):
     # data_red = np.column_stack((x_red, y_red))
     # np.savetxt(final_txt_file_loc,
     #             data_red, header='x_red x_red', fmt="%.6f")
-    exp_idx+=1
+    # exp_idx+=1
 plt.savefig(paths.figures / 'raw_data.pdf')
 print('The end')
 # make a tar file
