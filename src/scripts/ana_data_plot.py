8bddd7c0c1c728d0bc018319d431816bdee6923e
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-19 14:39:33 +0100

    changed analysis plot format

--- src/scripts/ana_data_plot.py
+++ src/scripts/ana_data_plot.py
@@ -93,10 +93,13 @@ try:
     txt_file_tar = tar.extractfile(txt_file_name)
     ana_data = np.loadtxt(txt_file_tar, comments='#', dtype=float)
     org_k=ana_data[:,0]
-    org_k=ana_data[:,1]
+    org_t=ana_data[:,1]
     fit_k=ana_data[:,2]
     fit_t=ana_data[:,3]
     plt.plot(org_k)
+    plt.plot(fit_k,'o')
+    plt.plot(org_t)
+    plt.plot(fit_t,'o')
     plt.savefig(paths.figures / 'ana_data.pdf')
     # # save reduced data
     # # reduced data dir for exp
