f139829c1751bb654a93a45ddc4292603ca37061
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-19 15:40:47 +0100

    added plot scrpt for raw data

--- src/scripts/raw_data_plot.py
+++ src/scripts/raw_data_plot.py
@@ -15,6 +15,8 @@ tar=tarfile.open(tar_path, 'r:gz')
 # read trail
 exp_idx=0
 
+plt.figure(figsize=(8, 4))
+
 while True:
     try:
         txt_file_path = tar.extractfile(f'data/exp_{exp_idx}/data.txt')
@@ -24,10 +26,14 @@ while True:
         x_noise=data[:,2]
         y_noise=data[:,3]
         # x_red, y_red=data_angle_cor(x_noise, y_noise, alpha_val)
+        label_name=f'Measurement {exp_idx}'
         plt.plot(x_noise,y_noise,'o')
         exp_idx += 1
     except KeyError:
         break
-
+plt.xlabel('Pixel positions [A.U.]', fontsize=15)
+plt.ylabel('Measurement amplitude [A.U.]', fontsize=15)
+plt.legend(fontsize=15, ncols=1, loc='center left', bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
 plt.savefig(paths.figures / 'raw_data.pdf')
 print('The end')
