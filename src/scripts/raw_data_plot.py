ddeb28998287090a6077fd95b42de5c94f2bd9a3
Arnab Majumdar <arnab.majumdar@hereon.de>
2026-03-20 17:12:50 +0100

    code readability: raw data plot

--- src/scripts/raw_data_plot.py
+++ src/scripts/raw_data_plot.py
@@ -1,5 +1,6 @@
 """
-Python files to read dummy dataset created by data_create.py
+Plot script for plotting raw data
+Raw data doi: 10.5281/zenodo.19115970
 """
 
 import os
@@ -8,29 +9,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import paths
 
-# open tarfile
+# open raw data tarfile
 tar_path=paths.data / 'data.tar.gz'
 tar=tarfile.open(tar_path, 'r:gz')
 
-# read trail
+# read raw data for all measurements (in this case 10)
 exp_idx=0
-
+# initialize plot figure
 plt.figure(figsize=(8, 4))
-
+# loop until measurement data exist
 while True:
     try:
+        # text file containing measured  data
         txt_file_path = tar.extractfile(f'data/exp_{exp_idx}/data.txt')
         data = np.loadtxt(txt_file_path, comments='#', dtype=float)
-        x=data[:,0]
-        y=data[:,1]
-        x_noise=data[:,2]
-        y_noise=data[:,3]
-        # x_red, y_red=data_angle_cor(x_noise, y_noise, alpha_val)
+        # data read
+        raw_x=data[:,2]
+        raw_y=data[:,3]
+        # plotting raw data
         label_name=f'Measurement {exp_idx}'
-        plt.plot(x_noise,y_noise,'o', label=label_name)
+        plt.plot(raw_x,raw_y,'o', label=label_name)
         exp_idx += 1
     except KeyError:
         break
+# plot formatting 
 plt.xlabel('Pixel positions [A.U.]', fontsize=15)
 plt.ylabel('Measurement amplitude [A.U.]', fontsize=15)
 plt.xticks(fontsize=15)
@@ -38,4 +40,3 @@ plt.yticks(fontsize=15)
 plt.legend(fontsize=15, ncols=1, loc='center left', bbox_to_anchor=(1, 0.5))
 plt.tight_layout()
 plt.savefig(paths.figures / 'raw_data.pdf')
-print('The end')
